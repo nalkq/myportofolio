b8710d3a900c76642ad26ba8a73633c144740e9c
Kaysan <navidkaysan@gmail.com>
2026-09-14 20:08:48 +0700

    feat: add experience section

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Mahasiswa
 from .models import Skill
+from .models import Experience
 
 @admin.register(Mahasiswa)
 class MahasiswaAdmin(admin.ModelAdmin):
     list_display = ('nama', 'npm')
     
 admin.site.register(Skill)
+admin.site.register(Experience)
